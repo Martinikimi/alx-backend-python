a7e0709e897b57a4b939ecf0adda4cc1e54fa560
Martin Kimani <martinikimi7@gmail.com>
2025-08-17 17:40:53 +0300

    Add task_4.sql to show full description of books table

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Info'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
+
+
