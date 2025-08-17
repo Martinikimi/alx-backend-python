cf00fdc9154e14a79409126ceebdd95334061edc
Martin Kimani <martinikimi7@gmail.com>
2025-08-17 18:13:07 +0300

    Add task_6.sql to insert multiple customers

--- task_6.sql
+++ task_6.sql
@@ -1,22 +1,11 @@
 -- Use the correct database
 USE alx_book_store;
 
--- Insert multiple customers
-INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES
-(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'),
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-
-USE alx_book_store;
-
-INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES
-(5, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'),
-(6, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(7, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(8, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Insert multiple customers safely
+INSERT INTO customers (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
 
 
