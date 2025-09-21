bdfb1a97db48b227daa6b447366cfa5255cb3424
Martin Kimani <martinikimi7@gmail.com>
2025-09-21 20:55:10 +0300

    Delete messaging_app/messaging_app/chats directory

--- messaging_app/messaging_app/chats/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Message
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'sender', 'receiver', 'timestamp', 'is_read']
-    list_filter = ['timestamp', 'is_read']
-    search_fields = ['sender__username', 'receiver__username', 'content']
-    date_hierarchy = 'timestamp'
