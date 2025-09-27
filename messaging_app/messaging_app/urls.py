f477a1503353c4f2e0ec43d53f3b4c2f9bfc1cfa
Martin Kimani <martinikimi7@gmail.com>
2025-09-27 16:32:37 +0300

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,9 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from chats import auth
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/chats/', include('chats.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+
+
+
