3491ce17e8e22111e1eff1f8960081a7e9c65d94
Martin Kimani <martinikimi7@gmail.com>
2025-09-27 16:39:04 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -217,3 +217,30 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
         
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+from rest_framework import viewsets, permissions
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated, IsParticipantOfConversation]
+
+    def get_queryset(self):
+        # Limit conversations to only those the user participates in
+        return Conversation.objects.filter(participants=self.request.user)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated, IsParticipantOfConversation]
+
+    def get_queryset(self):
+        # Limit messages to only conversations the user participates in
+        return Message.objects.filter(conversation__participants=self.request.user)
+
+
