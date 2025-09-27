295391f769a45ef19a44eae02e196bc4a9e7dbb5
Martin Kimani <martinikimi7@gmail.com>
2025-09-27 17:14:17 +0300

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -26,6 +26,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
      "chats.middleware.RequestLoggingMiddleware",
     "chats.middleware.RestrictAccessByTimeMiddleware",
+    "chats.middleware.OffensiveLanguageMiddleware",
 
 ] 
 ROOT_URLCONF = "Django-Middleware-0x03.urls"
