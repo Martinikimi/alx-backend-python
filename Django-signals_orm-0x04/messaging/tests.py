ab72f742f53f562f0b66ecb8c283001915f197c3
Martin Kimani <martinikimi7@gmail.com>
2025-10-05 19:05:05 +0300

    Update tests.py

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,1 +1,31 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification, MessageHistory
 
+class MessageSignalTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user('user1', 'user1@test.com', 'password')
+        self.user2 = User.objects.create_user('user2', 'user2@test.com', 'password')
+
+    def test_notification_created_on_message_save(self):
+        message = Message.objects.create(
+            sender=self.user1,
+            receiver=self.user2,
+            content="Test message"
+        )
+        self.assertEqual(Notification.objects.count(), 1)
+        notification = Notification.objects.first()
+        self.assertEqual(notification.user, self.user2)
+        self.assertEqual(notification.message, message)
+
+    def test_message_history_on_edit(self):
+        message = Message.objects.create(
+            sender=self.user1,
+            receiver=self.user2,
+            content="Original content"
+        )
+        message.content = "Edited content"
+        message.save()
+        self.assertEqual(MessageHistory.objects.count(), 1)
+        history = MessageHistory.objects.first()
+        self.assertEqual(history.old_content, "Original content")
